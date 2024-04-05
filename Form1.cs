3ee693326abdd83b2d5662a4b8d324b80632521d
Amazing Cabiles <amazinggracecabiles@gmail.com>
2024-04-05 21:22:15 +0800

    Added Currency

--- Form1.cs
+++ Form1.cs
@@ -72,5 +72,79 @@ namespace NotesApp
             NoteBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
             editing = true;
         }
+
+        private void ConverterButton_Click(object sender, EventArgs e)
+        {
+            Converter converterForm = new Converter();
+            converterForm.Show();
+        }
+
+        // Change color of the converter button on hover
+        private void ConverterButton_MouseHover(object sender, EventArgs e)
+        {
+            ConverterButton.BackColor = Color.FromArgb(114, 137, 218);
+        }
+
+        // Revert to default color of the converter button
+        private void ConverterButton_OnMouseLeave(object sender, EventArgs e)
+        {
+            ConverterButton.ResetBackColor();
+            ConverterButton.UseVisualStyleBackColor = true;
+        }
+
+        private void LoadButton_MouseHover(object sender, EventArgs e)
+        {
+            LoadButton.BackColor = Color.FromArgb(114, 137, 218);
+        }
+
+        private void LoadButton_MouseLeave(object sender, EventArgs e)
+        {
+            LoadButton.ResetBackColor();
+            LoadButton.UseVisualStyleBackColor = true;
+        }
+
+        private void NewButton_MouseHover(object sender, EventArgs e)
+        {
+            NewButton.BackColor = Color.FromArgb(114, 137, 218);
+        }
+
+        private void NewButton_MouseLeave(object sender, EventArgs e)
+        {
+            NewButton.ResetBackColor();
+            NewButton.UseVisualStyleBackColor = true;
+        }
+
+        private void DeleteButton_MouseHover(object sender, EventArgs e)
+        {
+            DeleteButton.BackColor = Color.FromArgb(114, 137, 218);
+        }
+
+        private void DeleteButton_MouseLeave(object sender, EventArgs e)
+        {
+            DeleteButton.ResetBackColor();
+            DeleteButton.UseVisualStyleBackColor = true;
+        }
+
+        private void SaveButton_MouseHover(object sender, EventArgs e)
+        {
+            SaveButton.BackColor = Color.FromArgb(114, 137, 218);
+        }
+
+        private void SaveButton_MouseLeave(object sender, EventArgs e)
+        {
+            SaveButton.ResetBackColor();
+            SaveButton.UseVisualStyleBackColor = true;
+        }
+
+        private void TimerButton_MouseHover(object sender, EventArgs e)
+        {
+            TimerButton.BackColor = Color.FromArgb(114, 137, 218);
+        }
+
+        private void TimerButton_MouseLeave(object sender, EventArgs e)
+        {
+            TimerButton.ResetBackColor();
+            TimerButton.UseVisualStyleBackColor = true;
+        }
     }
 }
