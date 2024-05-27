0ffc00a49e3fdcb8c6c991bc00ac3cffd024631c
Cyrelle Kristin Gapit <cyrellekristingapit12@gmail.com>
2024-05-27 14:26:42 +0800

    changing color of day calendar (amaze)

--- obj/Debug/net8.0-windows/NotesApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NotesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeSMART")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed17e46d8b7181b3fe3908c08a6d1f9cd885fc23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc5e7f6013e8ca976a3cfdb98026a1377fd06a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeSMART")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeSMART")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
